0fd7362584129e3e29509a79675cd620a150ff52
Lawrence LCI <31262254+lcheunglci@users.noreply.github.com>
2025-11-05 06:35:23 +0000

    Add some page components and routing

--- otg-web-angular/src/app/app.routes.ts
+++ otg-web-angular/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  {
+    path: 'welcome',
+    loadComponent: () => import('./page/welcome/welcome.component').then((m) => m.WelcomeComponent),
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./page/about/about.component').then((m) => m.AboutComponent),
+  },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./page/products/products.component').then((m) => m.ProductsComponent),
+  },
+];
