81b946083bd34f58e77cffb5e7dc1abb2ff0d3d9
Lawrence LCI <31262254+lcheunglci@users.noreply.github.com>
2025-11-15 10:06:55 -0800

    Update sidebar.component.ts

--- otg-web-angular/src/app/components/sidebar/sidebar.component.ts
+++ otg-web-angular/src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
-
+  items = input<strong[]>([]);
 }
