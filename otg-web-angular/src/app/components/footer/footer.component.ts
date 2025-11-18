1cf7d84baa074c4b177f2879b11a4c5320640db3
Lawrence LCI <31262254+lcheunglci@users.noreply.github.com>
2025-11-18 03:02:47 -0800

    Update footer.component.ts

--- otg-web-angular/src/app/components/footer/footer.component.ts
+++ otg-web-angular/src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+    showSiteMap = input(false);
 }
