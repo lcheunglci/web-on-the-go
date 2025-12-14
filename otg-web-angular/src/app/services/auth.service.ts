524a6394f204b1aa12d6bc3c53691f6fbeeb73e8
Lawrence LCI <lcheunglci@gmail.com>
2025-12-13 23:55:53 -0800

    Update the auth service

--- otg-web-angular/src/app/services/auth.service.ts
+++ otg-web-angular/src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { httpResource } from '@angular/common/http';
-import { Injectable, signal } from '@angular/core';
+import { computed, Injectable, linkedSignal, signal } from '@angular/core';
 import { User } from '../models/user.model';
 
 @Injectable({
@@ -7,20 +7,36 @@ import { User } from '../models/user.model';
 })
 export class AuthService {
   readonly userId = signal<string | undefined>(undefined);
+  readonly loginError = signal<boolean>(false);
+
+  allUsers = httpResource<User[]>(() => '/api/users');
 
   authenticatedUser = httpResource<User>(() => `/api/users/${this.userId()}`);
 
+  userPermission = computed(() => {
+    return this.userId() ? this.authenticatedUser.value()?.permissions || [] : [];
+  });
+
+  validUserIds = linkedSignal(() => {
+    return this.allUsers.hasValue() ? this.allUsers.value().map((user) => user.id) : [];
+  });
+
   isAuthorized(requiredPermission: any) {
-    // TODO: add permission
-    return true;
+    return !!this.authenticatedUser.value()?.permissions.includes(requiredPermission);
   }
 
   login(userId: string) {
     // check if the user is valid
-    this.userId.set(userId.toLowerCase());
+    if (this.validUserIds().includes(userId.toLowerCase())) {
+      this.userId.set(userId.toLowerCase());
+      this.loginError.set(false);
+    } else {
+      this.loginError.set(true);
+    }
   }
 
   logout() {
     this.userId.set(undefined);
+    this.loginError.set(false);
   }
 }
