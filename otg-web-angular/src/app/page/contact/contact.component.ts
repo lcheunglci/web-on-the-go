0aa24c99e34fd9c0a5e902048ccf512e7db45625
Lawrence LCI <lcheunglci@gmail.com>
2025-12-25 23:33:46 -0800

    Update contact component

--- otg-web-angular/src/app/page/contact/contact.component.ts
+++ otg-web-angular/src/app/page/contact/contact.component.ts
@@ -1,9 +1,60 @@
-import { Component } from '@angular/core';
+import { ContactService } from './../../services/contact.service';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ContactForm } from '../../models/contact-form';
+import { ReplaySubject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
-export class ContactComponent {}
+export class ContactComponent {
+  readonly contactService = inject(ContactService);
+
+  destroyed$ = new ReplaySubject<void>(1);
+
+  model: ContactForm = {
+    fullName: '',
+    email: '',
+    phone: '',
+    comment: '',
+  };
+
+  submitted = false;
+  loading = false;
+
+  submitForm(model: ContactForm) {
+    this.submitted = true;
+    this.loading = true;
+
+    this.contactService
+      .submitContactForm(model)
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(() => {
+        this.contactService.canDeactivate.set(true);
+        this.loading = false;
+      });
+  }
+
+  clearForm() {
+    this.contactService.canDeactivate.set(true);
+    this.submitted = false;
+    this.model = {
+      fullName: '',
+      email: '',
+      phone: '',
+      comment: '',
+    };
+  }
+
+  ngOnInit() {
+    this.contactService.canDeactivate.set(false);
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
+}
